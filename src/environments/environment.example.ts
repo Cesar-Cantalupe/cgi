37ab772bad8ad5c973b238d4381f944e08295200
mrestom <marcorestom@gmail.com>
2026-02-25 12:56:33 -0300

    +

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  websocketUrl: 'wss://d2nrwi7jko6xiv.cloudfront.net/ws/query',
+  websocketUrl: 'wss://URL_DEV/ws/query',
   apiKey: 'API_KEY_DEV',
 };
